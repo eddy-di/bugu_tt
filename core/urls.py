fe3b7f7bcbd750ae83316e237c9ffbb740f69b48
eddy.di <eddy.di.fint@gmail.com>
2024-07-31 15:33:44 +0600

    core url update, readme update

--- core/urls.py
+++ core/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('articles/', include('articles.urls')),
-    path('', SpectacularAPIView.as_view(), name='schema'),
+    path('schema', SpectacularAPIView.as_view(), name='schema'),
     path('swagger-ui', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('redoc', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
