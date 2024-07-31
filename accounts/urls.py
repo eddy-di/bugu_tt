765220e10894e1f3ecda7f6324be7797d5bf73a5
eddy.di <eddy.di.fint@gmail.com>
2024-08-01 02:45:38 +0600

    update accounts urls, and env.example add secretkey

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ from accounts.views import LoginView, LogoutView, RegisterAuthorView, RegisterVi
 
 urlpatterns = [
     path('register', RegisterView.as_view(), name='register'),
-    path('register/author', RegisterAuthorView.as_view(), name='register'),
+    path('register/author', RegisterAuthorView.as_view(), name='register_author'),
     path('login', LoginView.as_view(), name='login'),
     path('logout', LogoutView.as_view(), name='logout'),
 ]
